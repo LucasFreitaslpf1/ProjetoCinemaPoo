2ff2f5da2463e1bafd3c56e4fb0dc0aae0cd9083
Dayany Lima Santos <dayanyylima@gmail.com>
2022-05-30 19:27:59 -0300

    Classe Sala adicionada juntamente com  enum Tela

---
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Sala.java
@@ -0,0 +1,57 @@
+package br.edu.ifnmg.projetocinemapoo.entity;
+
+public class Sala extends Entidade {
+
+    private Integer numero;
+    private Integer capacidade;
+    private Tela tela;
+
+    
+    //<editor-fold defaultstate="collapsed" desc="Construtores">
+    
+    public Sala() {
+    }
+
+    public Sala(Integer numero, Integer capacidade, Tela tela) {
+        this.numero = numero;
+        this.capacidade = capacidade;
+        this.tela = tela;
+    }
+    
+    //</editor-fold>
+    
+    //<editor-fold defaultstate="collapsed" desc=" Getters/Setters">
+    
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Integer getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public Tela getTela() {
+        return tela;
+    }
+
+    public void setTela(Tela tela) {
+        this.tela = tela;
+    }
+    
+    //</editor-fold>
+
+    @Override
+    public String toString() {
+        return "Sala{" + "numero=" + numero + ", capacidade=" + capacidade + ", tela=" + tela + '}';
+    }
+    
+    
+}
---
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Tela.java
@@ -0,0 +1,8 @@
+package br.edu.ifnmg.projetocinemapoo.entity;
+
+public enum Tela {
+
+    _3D,
+    _2D;
+
+}
