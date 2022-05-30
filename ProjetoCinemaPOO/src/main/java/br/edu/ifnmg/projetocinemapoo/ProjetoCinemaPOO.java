5ba6bcc5d5735f14c5a2a3647b3ae75dbce6f773
Dayany Lima Santos <dayanyylima@gmail.com>
2022-05-30 19:02:21 -0300

    Teste de acesso a repositório

--- ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/ProjetoCinemaPOO.java
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/ProjetoCinemaPOO.java
@@ -12,6 +12,6 @@ package br.edu.ifnmg.projetocinemapoo;
 public class ProjetoCinemaPOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!!");
     }
 }
