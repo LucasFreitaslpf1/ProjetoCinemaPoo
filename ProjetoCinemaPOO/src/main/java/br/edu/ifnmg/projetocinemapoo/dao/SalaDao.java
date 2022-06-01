e9b0d88e347f05df5465bd649262ccdb190ef287
Dayany Lima Santos <dayanyylima@gmail.com>
2022-06-01 01:20:08 -0300

    Implementada Classe SalaDao

---
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/dao/SalaDao.java
@@ -0,0 +1,90 @@
+package br.edu.ifnmg.projetocinemapoo.dao;
+
+import br.edu.ifnmg.projetocinemapoo.entity.Sala;
+import br.edu.ifnmg.projetocinemapoo.entity.Tela;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SalaDao extends Dao<Sala, Long> {
+
+    @Override
+    public String obterSentencaInsert() {
+        return "insert into sala (id, numero, capacidade, tela) values (default, ? , ? , ?); ";
+    }
+
+    @Override
+    public String obterSentencaUpdate() {
+        return "update sala set numero = ?, capacidade = ?, tela = ? where id = ?;";
+    }
+
+    @Override
+    public void montarDeclaracao(PreparedStatement pstmt, Sala e) {
+        try {
+
+            pstmt.setInt(1, e.getNumero());
+            pstmt.setInt(2, e.getCapacidade());
+            pstmt.setString(3, e.getTela());
+
+            if (e.getId() != null && e.getId() != 0) {
+                pstmt.setLong(4, e.getId());
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SalaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public String obterSentencaLocalizarPorId() {
+        return "SELECT * FROM sala WHERE id = ?";
+    }
+
+    @Override
+    public Sala extrairObjeto(ResultSet resultSet) {
+        Sala sala = new Sala();
+
+        try {
+
+            sala.setId(resultSet.getLong("id"));
+            sala.setNumero(resultSet.getInt("numero"));
+            sala.setCapacidade(resultSet.getInt("capacidade"));
+            sala.setTela((Tela) resultSet.getObject("tela"));
+          
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SalaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return sala;
+    }
+
+    @Override
+    public String obterSentencaLocalizarTodos() {
+        return "select id, numero, capacidade, tela from sala;";
+    }
+
+    @Override
+    public List<Sala> extrairObjetos(ResultSet rs) {
+
+        ArrayList<Sala> salas = new ArrayList<>();
+
+        try {
+
+            while (rs.next()) {
+
+                salas.add(extrairObjeto(rs));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SalaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return salas;
+    }
+
+}
