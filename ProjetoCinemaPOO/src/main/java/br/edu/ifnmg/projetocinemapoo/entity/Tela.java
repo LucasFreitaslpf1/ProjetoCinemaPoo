9694f35c07d4eed51d206a8d9f50aee3ea7821b5
Dayany Lima Santos <dayanyylima@gmail.com>
2022-06-01 01:21:00 -0300

    Enum tela modificado

--- ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Tela.java
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Tela.java
@@ -2,7 +2,21 @@ package br.edu.ifnmg.projetocinemapoo.entity;
 
 public enum Tela {
 
-    _3D,
-    _2D;
+    _2D {
+
+        @Override
+        public String getDescription() {
+            return "2D";
+        }
+    },
+    _3D {
+
+        @Override
+        public String getDescription() {
+            return "3D";
+        }
+    };
+
+    public abstract String getDescription();
 
 }
