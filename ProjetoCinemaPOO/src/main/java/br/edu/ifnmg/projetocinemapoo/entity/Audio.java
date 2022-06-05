66f9d1dac373d42da1d8c65ad49f3c8f43f27fcc
Dayany Lima Santos <dayanyylima@gmail.com>
2022-06-05 19:03:00 -0300

    Realizadas alterações

--- ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Audio.java
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Audio.java
@@ -1,6 +1,21 @@
 package br.edu.ifnmg.projetocinemapoo.entity;
 
 public enum Audio {
-    LEGENDADO,
-    DUBLADO;
+    // LEGENDADO,DUBLADO;
+    LEGENDADO {
+
+        @Override
+        public String getDescription() {
+            return "LEGENDADO";
+        }
+    },
+    DUBLADO {
+
+        @Override
+        public String getDescription() {
+            return "DUBLADO";
+        }
+    };
+
+    public abstract String getDescription();
 }
