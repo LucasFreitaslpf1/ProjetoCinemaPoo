a25b0b96528f5e74928040fc587c8f46d3dfaa2d
Lucas <Lucas@LAPTOP-J88O2G22>
2022-06-06 01:47:54 -0300

    Correção Sessao e adição de SessaoDao

---
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/dao/SessaoDao.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifnmg.projetocinemapoo.dao;
+
+import br.edu.ifnmg.projetocinemapoo.entity.Audio;
+import br.edu.ifnmg.projetocinemapoo.entity.Sessao;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lucas
+ */
+public class SessaoDao extends Dao<Sessao,Long> {
+
+    @Override
+    public String obterSentencaInsert() {
+        return "insert into sessao(horario,audio,ingressosvendidos,valorsessao) values ( ?, ?, ?, ?);";
+    }
+
+    @Override
+    public String obterSentencaUpdate() {
+        return "update sessao set horario = ?, audio = ?, ingressosvendidos = ?, valorsessao = ? where id = ?;";
+    }
+
+    @Override
+    public String obterSentencaLocalizarPorId() {
+        return "select id,horario,audio,ingressosvendidos,valorsessao from sessao where id = ?;";
+    }
+
+    @Override
+    public Sessao extrairObjeto(ResultSet resultSet) {
+        Sessao s = new Sessao();
+
+        try {
+            s.setId(resultSet.getLong("id"));
+            s.setHorario(resultSet.getObject("horario", LocalDateTime.class));
+            s.setAudio(Audio.valueOf(resultSet.getString("audio")));
+            s.setIngressosVendidos(resultSet.getInt("ingressosvendidos"));
+            s.setValorSessao(resultSet.getDouble("valorsessao"));
+        } catch (SQLException ex) {
+            Logger.getLogger(SessaoDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Ex = " + ex);
+        }
+        return s;
+    }
+
+    @Override
+    public String obterSentencaLocalizarTodos() {
+        return "select id,horario,audio,ingressosvendidos,valorsessao from sessao;";
+    }
+
+    @Override
+    public List extrairObjetos(ResultSet rs) {
+        ArrayList<Sessao> sessoes = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                sessoes.add(extrairObjeto(rs));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(SessaoDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return sessoes;
+    }
+
+    @Override
+    public void montarDeclaracao(PreparedStatement pstmt, Sessao e) {
+        try {
+            if (e.getId() == null || e.getId() == 0) {
+                pstmt.setString(1, e.getHorario().toString());
+                pstmt.setString(2, e.getAudio());
+                pstmt.setInt(3, e.getIngressosVendidos());
+                pstmt.setDouble(4, e.getValorSessao());
+            } else {
+                pstmt.setString(1, e.getHorario().toString());
+                pstmt.setString(2, e.getAudio());
+                pstmt.setInt(3, e.getIngressosVendidos());
+                pstmt.setDouble(4, e.getValorSessao());
+                pstmt.setLong(5, e.getId());
+            }
+        } catch (Exception ex) {
+            System.out.println("Exceptoin: " + ex);
+        }
+    }
+}
--- ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Sessao.java
+++ ProjetoCinemaPOO/src/main/java/br/edu/ifnmg/projetocinemapoo/entity/Sessao.java
@@ -4,35 +4,23 @@ import java.time.LocalDateTime;
 
 public class Sessao extends Entidade {
 
-    private Filme filme;
     private LocalDateTime horario;
-    private Sala sala;
     private Audio audio;
-    private Short ingressosVendidos;
+    private Integer ingressosVendidos;
     private Double valorSessao;
 
     //<editor-fold defaultstate="collapsed" desc="Constructors">
     public Sessao() {
     }
 
-    public Sessao(Filme filme, LocalDateTime horario, Sala sala, Audio audio, Short ingressosVendidos, Double valorSessao) {
-        this.filme = filme;
+    public Sessao(LocalDateTime horario, Audio audio, Integer ingressosVendidos, Double valorSessao) {
         this.horario = horario;
-        this.sala = sala;
         this.audio = audio;
         this.ingressosVendidos = ingressosVendidos;
         this.valorSessao = valorSessao;
     }
-
-    //</editor-fold>
+//</editor-fold>
     //<editor-fold defaultstate="collapsed" desc="Getters/Setters">
-    public Filme getFilme() {
-        return filme;
-    }
-
-    public void setFilme(Filme filme) {
-        this.filme = filme;
-    }
 
     public LocalDateTime getHorario() {
         return horario;
@@ -40,14 +28,6 @@ public class Sessao extends Entidade {
 
     public void setHorario(LocalDateTime horario) {
         this.horario = horario;
-    }
-
-    public Sala getSala() {
-        return sala;
-    }
-
-    public void setSala(Sala sala) {
-        this.sala = sala;
     }
 
     public String getAudio() {
@@ -58,11 +38,11 @@ public class Sessao extends Entidade {
         this.audio = audio;
     }
 
-    public Short getIngressosVendidos() {
+    public Integer getIngressosVendidos() {
         return ingressosVendidos;
     }
 
-    public void setIngressosVendidos(Short ingressosVendidos) {
+    public void setIngressosVendidos(Integer ingressosVendidos) {
         this.ingressosVendidos = ingressosVendidos;
     }
 
@@ -72,12 +52,12 @@ public class Sessao extends Entidade {
 
     public void setValorSessao(Double valorSessao) {
         this.valorSessao = valorSessao;
-    }
+    }    
+//</editor-fold>
 
-    //</editor-fold>
     @Override
     public String toString() {
-        return "Sessao{" + "filme=" + filme + ", horario=" + horario + ", sala=" + sala + ", audio=" + audio + ", ingressosVendidos=" + ingressosVendidos + ", valorSessao=" + valorSessao + '}';
+        return "Sessao{" + "horario=" + horario + ", audio=" + audio + ", ingressosVendidos=" + ingressosVendidos + ", valorSessao=" + valorSessao + '}';
     }
 
 }
